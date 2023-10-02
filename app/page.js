94aa9177838607f69bee1a1fa35af9c14207f5d2
azubair2012 <studio.zubair@gamil.com>
2023-10-03 00:53:44 +0600

    s

--- app/page.js
+++ app/page.js
@@ -1,9 +1,47 @@
+"use client";
+import { useState } from "react";
 import Image from "next/image";
+import { GoPeople, GoHome, GoMail, GoGear, GoCreditCard } from "react-icons/go";
 
 export default function Home() {
+  const [Display, setDisplay] = useState();
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>Hi</div>
+    <main className="bg-[url('/img/pexels-terje-sollie-312029.jpg')] bg-cover h-screen flex justify-center items-center gap-20">
+      <div className="text-white text-4xl border-[1px] border-[#cecbc3] rounded-full h-fit w-fit flex flex-col items-center justify-evenly px-4 py-4 gap-4 overflow-hidden backdrop-blur-sm">
+        <GoHome
+          className="cursor-pointer"
+          onClick={() => {
+            setDisplay("Home");
+          }}
+        />
+        <GoPeople
+          className="cursor-pointer"
+          onClick={() => {
+            setDisplay("People");
+          }}
+        />
+        <GoMail
+          className="cursor-pointer"
+          onClick={() => {
+            setDisplay("Mail");
+          }}
+        />
+        <GoCreditCard
+          className="cursor-pointer"
+          onClick={() => {
+            setDisplay("Cards");
+          }}
+        />
+        <GoGear
+          className="cursor-pointer"
+          onClick={() => {
+            setDisplay("Settings");
+          }}
+        />
+      </div>
+      <div className="text-white text-4xl border-[1px] border-[#cecbc3] rounded-3xl h-[780px] w-[900px] backdrop-blur-md ">
+        {Display}
+      </div>
     </main>
   );
 }
